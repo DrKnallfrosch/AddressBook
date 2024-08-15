5824414070a877c2d993597a2a15dd822317e417
Tim <schuettetim005@gmail.com>
2024-08-15 23:37:14 +0200

    change dataclass to pydantic.dataclasses.dataclass
    ----------------------------------
    Todo:
    Implementation email validation

--- AddressBook.py
+++ AddressBook.py
@@ -1,39 +1,48 @@
-from dataclasses import dataclass
 from datetime import datetime, date
+from pydantic.dataclasses import dataclass
+from pydantic import field_validator, PositiveInt, validate_email
+from typing import Optional
+from dataclasses import field
+
 
 @dataclass(order=True)
-class Address:
+class AddressBook:
     firstname: str
     lastname: str
-    street: str = ''
-    number: str = ''
-    postal_code: int = 0 
-    place: str = ''
-    birthday: date = '0001-01-01'
-    phone: str = ""
-    email: str = ""
+    street: Optional[str] = field(default=None)
+    number: Optional[str] = field(default=None)
+    postal_code: Optional[PositiveInt] = field(default=None)
+    place: Optional[str] = field(default=None)
+    birthdate: Optional[str] = field(default=None)
+    phone: Optional[str] = field(default=None)
+    email: Optional[str] = field(default=None)
 
-    def __post_init__(self):
-        self.birthday: date = datetime.strptime(self.birthday, "%Y-%m-%d").date()
+    @field_validator('birthdate')
+    def validate_birthdate(cls, value) -> date:
+        return datetime.strptime(value, "%Y-%m-%d").date()
+
+    # TODO
+    @field_validator('email')
+    def validate_email(cls, value):
+        pass
 
     def __str__(self):
-        return f"Name: {self.firstname} {self.lastname}\nAddress: {self.street} {self.number}, {self.postal_code} {self.place}\nContact: {self.phone} {self.email}\n"
-
-
-
-
+        return (f"Name: {self.firstname} {self.lastname}\nAddress: {self.street} {self.number}, {self.postal_code} "
+                f"{self.place}\nContact: {self.phone} {self.email}\n")
 
 
 if __name__ == '__main__':
-    a1 = Address('Henri', 'Henrison', 'ABC-Street', '10', 11111, 'Berlin', '2001-12-01', '12345678', 'a@mail.com')
-    a2 = Address('Henri', 'Henrison', 'ABC-Street', '12a', 11111, 'Berlin')
-    a3 = Address('Henri', 'Henrison', 'ABC-Street', '10', 11111, 'Berlin', '2001-12-01', '12345678', 'a@mail.com')
-    
+    a1 = AddressBook('Henri', 'Henrison', 'ABC-Street', '10', 11111, 'Berlin', '2001-12-01', '12345678',
+                     'a@mail.com')
+    a2 = AddressBook('Henri', 'Henrison', 'ABC-Street', '12a', 11111, 'Berlin')
 
-    
+    a3 = AddressBook('Henri', 'Henrison', 'ABC-Street', '10', 11111, 'Berlin', '2001-12-01', '12345678',
+                     'a@mail.com')
+
     print(a1)
     print(a2)
     print(a1 > a2)
+
     l = [a1, a2, a3]
     sorted_list = sorted(l)
     for element in sorted_list:

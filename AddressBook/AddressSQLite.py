52ab3ad39201abb9f7d8354c28dfba972595caaf
d0ms_ <dominik.elias.hase@gmail.com>
2024-09-17 23:09:07 +0200

    implemented all except save no tests done

--- AddressBook/AddressSQLite.py
+++ AddressBook/AddressSQLite.py
@@ -1,79 +1,84 @@
-import sqlite3
-import tkinter as tk
-from tkinter import filedialog
-
-import AddressBook
 from AddressContainerInterface import AddressContainerInterface
+import AddressBook
+import sqlite3
 
 
 class AddressSQLite(AddressContainerInterface):
-    def __init__(self, mode=True, table="AddressBook"):
-        self.filepath = self.set_filepath(mode)
-        self.conn = sqlite3.connect(self.filepath)
-        self.cursor = self.conn.cursor()
-        self.table = table
+    def __init__(self, filepath:str = None, tablename="AddressBook"):
 
-    def set_filepath(self, mode: bool = 1):
-        root = tk.Tk()
-        root.withdraw()
+        self.filepath = filepath
+        self.conn = None
+        self.cursor = None
+        self.tablename = tablename
 
-        if mode:
-            filepath = filedialog.asksaveasfilename(
-                defaultextension=".db",
-                filetypes=[("SQLite database files", "*.db"), ("All files", "*.*")]
-            )
-            print(f"Created file at {filepath}")
+    def set_filepath(self, filepath: str):
+        self.filepath = filepath
 
-        else:
-            filepath = filedialog.askopenfilename(
-                title="Select SQLite Database File",
-                filetypes=[("SQLite Database Files", "*.db *.sqlite *.sqlite3"), ("All Files", "*.*")]
-            )
-            print(f"Opened file {filepath}")
-        return filepath
+    def open(self):
+        with open(self.filepath, "a"):
+            try:
+                self.conn = sqlite3.connect(self.filepath)
+                self.cursor = self.conn.cursor()
+            except sqlite3.Error as e:
+                print(f"Error Code {e.sqlite_errorcode}: {e.sqlite_errorname}")
 
-    def read(self):
-        pass
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
     def save(self):
-        pass
+        pass # might need implementation
 
-    def search(self, search_string: str, columns: list = ["all"], exact: bool = 0) -> dict:
-        if columns == ["all"]:
-            columns = ["id", "first_name", "last_name", "street", "number",
-                       "postal_code", "place", "birthdate", "phone", "email"]
+    def search(self, search_string: str) -> dict:
+        columns = ["id", "first_name", "last_name", "street", "number",
+                   "postal_code", "place", "birthdate", "phone", "email"]
         columns = f" LIKE '%{search_string}%' OR ".join(columns)
-        self.cursor.execute(f"SELECT * FROM {self.table} WHERE {columns} LIKE '%{search_string}%';")
-        data = {}
-        print(self.cursor.fetchall()[0])
-        for i in self.cursor.fetchall():
-            data.update({i[0]: i[1:]})
-        return data
+        self.cursor.execute(f"SELECT * FROM {self.tablename} WHERE {columns} LIKE '%{search_string}%';")
+        return {elements[0]: elements[1:] for elements in self.cursor.fetchall()}
 
-    def add_address(self, data) -> int:
+    def delete(self, address_id: int) -> int:
+        try:
+            self.cursor.execute(f"DELETE FROM {self.tablename} WHERE id = {address_id}")
+            self.conn.commit()
+            return address_id
+        except sqlite3.Error:
+            return False
 
-        self.cursor.execute(f'''
-        INSERT INTO {self.table} (first_name, last_name, street, number, postal_code, place, birthdate, phone, email)
-        VALUES ()''')
+    def update(self, address_id: int, **kwargs) -> int:
+        fields = ', '.join([f"{key} = ?" for key in kwargs])
+        values = list(kwargs.values()) + [address_id]
+        try:
+            self.cursor.execute(f"UPDATE {self.tablename} SET {fields} WHERE id = ?", values)
+            self.conn.commit()
+            return address_id
+        except sqlite3.Error:
+            raise KeyError
 
-    def delete_address(self, address_id: int) -> int:
-        pass
-
-    def update_address(self, address_id: int, **kwargs) -> int:
-        pass
+    def add_address(self, data: tuple) -> int:
+        try:
+            self.cursor.execute(f'''
+                        INSERT INTO {self.tablename} (first_name, last_name, street, number, postal_code, place, birthdate, phone, email)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', data)
+            self.conn.commit()
+            return self.cursor.lastrowid
+        except sqlite3.Error:
+            return False
 
     def get_all(self) -> dict:
-        pass
+        self.cursor.execute(f"SELECT * FROM {self.tablename}")
+        return {elements[0]: elements[1:] for elements in self.cursor.fetchall()}
 
-    def get_address(self, address_id: int) -> AddressBook:
-        pass
+    def get(self, address_id: int) -> AddressBook:
+        self.cursor.execute(f"SELECT * FROM {self.tablename} WHERE id = {address_id}")
+        return {elements[0]: elements[1:] for elements in self.cursor.fetchall()}
 
-    def get_bd_today(self) -> dict:
-        pass
+    def get_todays_birthdays(self) -> dict:
+        self.cursor.execute(f"SELECT * FROM {self.tablename} WHERE strftime('%m-%d', birthdate) = strftime('%m-%d', 'now');")
+        return {elements[0]: elements[1:] for elements in self.cursor.fetchall()}
 
     def setup_tables(self):
         self.cursor.execute(f'''
-            CREATE TABLE IF NOT EXISTS {self.table} (
+            CREATE table IF NOT EXISTS {self.tablename} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL,
@@ -83,12 +88,10 @@ class AddressSQLite(AddressContainerInterface):
                 place TEXT,
                 birthdate TEXT,
                 phone TEXT,
-                email TEXT
-            );
+                email TEXT);
         ''')
         self.conn.commit()
 
-
-a = AddressSQLite(mode=0, table="AddressBook")
+a = AddressSQLite(filepath=r"..\AddressBook\SQLite\addressbook.mp4", tablename="AddressBook")
 a.setup_tables()
-print(a.search(input("Search string: ")))
+print(a.get_todays_birthdays())
